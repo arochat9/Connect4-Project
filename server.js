27a662697b41f9ec45c82dace3cc79fd30662705
Sam Westerman <westerhack@gmail.com>
2017-11-28 12:11:56 -0600

    <auto commit message>

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
-// const app = require('./app.js');
-const PORT = 3456;
-const http = require('http');
-const fs = require('fs');
-
-/******************************************************************************/
-
-const app = http.createServer(function(req, resp){
-   var file_to_serve;
-   if(req.url === '/client.css')
-      file_to_serve = 'client.css';
-   else
-      file_to_serve = 'client.html';
-
-   fs.readFile(file_to_serve, function(err, data){
-      if(err)
-         return resp.writeHead(500);
-      resp.writeHead(200);
-      resp.end(data);
-   });
-});
-
-
-app.listen(PORT);
-
-// module.export = app;
+const app = require('./app.js');
+console.log(JSON.stringify(app));
 
 const socketio = require('socket.io');
 var io = socketio.listen(app);
