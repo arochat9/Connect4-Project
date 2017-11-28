401d8384540e086e4ea55fea54f050c727744c99
Sam Westerman <westerhack@gmail.com>
2017-11-28 12:46:31 -0600

    <auto commit message>

--- users.js
+++ users.js
@@ -4,24 +4,17 @@ class User {
       this.socket = socket;
    }
 }
-class Users {
-   constructor(){
-      this.users = {};
-   }
 
-   add_user(user){
-      var username = user.name;
-      if( username == false )
-         return `Invalid username ${username}`;
-      if( this.users[username] )
-         return `User '${username}' already is logged in.`;
-      this.users[username] = user;
-      return true;
-   }
+function add_user(user){
+   var username = user.name;
+   if( username == false )
+      return `Invalid username ${username}`;
+   if( this.users[username] )
+      return `User '${username}' already is logged in.`;
+   this.users[username] = user;
+   return true;
 }
 
-const users = new Users();
-module.exports = {
-   user: User,
-   users: users
-}
+const users = {};
+
+exports = { User: User, add_user: add_user };
