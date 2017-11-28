ed614aae4c938332c3916406bc84f6c0ae0052a5
Sam Westerman <westerhack@gmail.com>
2017-11-28 12:19:08 -0600

    <auto commit message>

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-const app = require('./app.js');
-console.log(JSON.stringify(app));
+const app = require('./app.js'),
+      socketio = require('socket.io'),
+      io = socketio.listen(app);
 
-const socketio = require('socket.io');
-var io = socketio.listen(app);
-console.log('a');
+var users = {}
 
 io.sockets.on('connection', function(socket){
-   socket.on('foo', (data) => {
-      data['bar'] = 99;
-      console.log(data);
-      socket.emit('bar', data);
+   socket.on('client-startup', () => {
+      send_game_types(socket);
+      send_games_list(socket);
    });
-   // socket.on('client-send-post',  (data) => { client_send_post(socket, data); });
-   // socket.on('client-send-post2',  (data) => { client_send_post2(socket, data); });
-   // socket.on('client-send-msg',  (data) => { client_send_msg(socket, data); });
-   // socket.on('client-login-attempt', (data) => { client_login_attempt(socket, data); });
-   // socket.on('client-kick-user', (data) => { client_kick_user(socket, data); });
-   // socket.on('client-ban-user', (data) => { client_ban_user(socket, data); });
-   // socket.on('client-logout', (_) => { client_disconnect(socket); });
-   // socket.on('client-startup', (_) => { client_startup(socket); });
-   // socket.on("disconnect", () => { client_disconnect(socket); });
 });
+
+console.log('Started up.');
